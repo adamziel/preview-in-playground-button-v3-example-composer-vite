0a8bb857a27e77bf012125e030318ef7fb2b465f
Adam Zieliński <adam@adamziel.com>
2026-05-28 17:57:07 +0100

    Fork smoke test v3 preview

--- playground-demo-plugin.php
+++ playground-demo-plugin.php
@@ -13,3 +13,5 @@ if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
 require_once __DIR__ . '/vendor/autoload.php';
 
 \PlaygroundDemo\Notice::register();
+
+// Fork smoke test: v3 preview workflows.
